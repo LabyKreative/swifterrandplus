9ac9b21e9d45395ae8e86c8a3a76b24a8cef4070
LabyKreative <labykreative@yahoo.com>
2023-08-13 18:57:37 +0100

    Footer work

--- app.py
+++ app.py
@@ -2,9 +2,11 @@
 Flask is a class that is used to instantiate web applications.
 """
 from flask import Flask, render_template
+from flask_fontawesome import FontAwesome
 
 
 app = Flask(__name__)
+fa = FontAwesome(app)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 
