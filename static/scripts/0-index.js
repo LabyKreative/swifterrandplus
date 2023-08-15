f85c269f707c9bac3a80275acb0206cc5cb133ed
LabyKreative <labykreative@yahoo.com>
2023-08-15 06:02:45 +0100

    Fix map err

--- static/scripts/0-index.js
+++ static/scripts/0-index.js
@@ -1,4 +1,4 @@
-// Read more button functionality for the index page (index.html) of the website.
+// Read more button functionality for the index page of the website.
 document
   .getElementById("readMoreButton")
   .addEventListener("click", function () {
@@ -13,3 +13,13 @@ document
       this.innerHTML = '<i class="bi bi-chevron-right"></i>Read More';
     }
   });
+
+// Create a script element for the Mapbox GL JS script
+mapboxgl.accessToken =
+  "pk.eyJ1IjoibGFieWtyZWF0aXZlIiwiYSI6ImNsOHUxa3JpYzBjd3Ezdm1kd3l4eHo2ZXcifQ.g5sskeC-FvlEFsZErNtIpA";
+var map = new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/mapbox/streets-v11",
+  center: [6.509779, 3.387539],
+  zoom: 4,
+});
