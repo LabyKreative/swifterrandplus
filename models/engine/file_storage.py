abe982c4eb6adf819b0fed11452e34ad8f199164
Jma85 <117782264+Jma85@users.noreply.github.com>
2023-08-24 19:49:39 +0000

    engine

---
+++ models/engine/file_storage.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+''' Defines the file storage class '''
+
+
+import json
+from models.base_model import BaseModel
+from models.user import Users
+from models.state import Agents
+from models.city import Createtask
+
