24abf934c3f7e6faca27f352cf901876d0ee9897
LabyKreative <labykreative@yahoo.com>
2023-09-04 19:07:20 +0100

    Implementing flask login

--- app.py
+++ app.py
@@ -1,32 +1,18 @@
-"""This module contains the main application code for the Flask app.
+"""
+Flask is a class that is used to instantiate web applications.
 """
 import os
 from dotenv import load_dotenv
 from flask import Flask, render_template, redirect, url_for, session
-from authlib.integrations.flask_client import OAuth
-from flask_dance.contrib.google import make_google_blueprint, google
 from flask_fontawesome import FontAwesome
 
-load_dotenv()
+load_dotenv()  # take environment variables from .env.
 
 app = Flask(__name__)
 app.secret_key = os.environ["APP_SECRET_KEY"]
-oauth = OAuth(app)
-
-hate_you_majay = os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
-
-
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 app.static_folder = "static"
 fa = FontAwesome(app)
-
-# Configure Google OAuth
-google_bp = make_google_blueprint(
-    client_id=os.environ['GOOGLE_CLIENT_ID'],
-    client_secret=os.environ['GOOGLE_CLIENT_SECRET'],
-    redirect_to='google.login'
-)
-app.register_blueprint(google_bp, url_prefix="/google_login")
 
 
 @app.route("/", strict_slashes=False)
@@ -35,35 +21,23 @@ def landing_page():
     return render_template("index.html")
 
 
-@app.route("/login")
+@app.route("/login", strict_slashes=False)
 def login():
-    """Initiate the Google OAuth login process."""
-    if not google.authorized:
-        return render_template("login.html")
-    else:
-        return redirect(url_for('user_dashboard'))
+    """Return the login page."""
+    return render_template("login.html")
 
 
-@app.route("/user_dashboard")
+@app.route("/user_dashboard", strict_slashes=False)
 def user_dashboard():
-    """Return the user's dashboard after successful login."""
-    if not google.authorized:
-        # If not authorized, redirect to the login page
-        return redirect(url_for('login'))
-    # Get user info from the OAuth provider (Google in this case)
-    resp = google.get('/userinfo')
-    user_info = resp.json()
-    # You can store the user_info in the session or use it as needed
-    session['user_info'] = user_info
-    # Render the user dashboard template
+    """Return the user to dashboard."""
     return render_template('user_dashboard.html')
 
 
 @app.route("/logout", strict_slashes=False)
 def logout():
     """Logout the user and clear session data."""
-    session.pop('google_oauth_token', None)
-    return redirect(url_for("landing_page"))
+    session.pop('profile', None)
+    return redirect(url_for('landing_page'))
 
 
 if __name__ == "__main__":
